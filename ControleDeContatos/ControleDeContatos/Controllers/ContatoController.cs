b645991c67de0e96c7ce0471983489fff8d5e7e9
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-07-29 16:53:05 -0300

    criando_front-end_do_sistema_de_cadastro_de_contatos

---
+++ ControleDeContatos/ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
